f96c09203246c465f176c67aa8f68faee277c6f2
CameronHCouch <cameronhcouch@gmail.com>
2019-06-18 16:05:57 -0400

    Implement initial solution.

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const dir = './images';
+
+fs.readdir(dir, (err, files) => {
+  console.log(files.length);
+});
+
+// grab carousel image from document, and locate its URL string
+let imageDiv = document.getElementById("image");
+let imageUrl = imageDiv.style.backgroundImage
+
+// determine which number image we are currently viewing
+let imageUrlNumber = parseInt(imageUrl[25]);
+
+(function rotateCarousel() {
+    // increment image approx every 3 seconds
+    // if image is largest image, reset to first image
+    setInterval(() => {
+        if (imageUrlNumber !== 3){
+            imageUrlNumber++;
+        } else {
+            imageUrlNumber = 0;
+        }
+        // replace the current imageUrlNumber with new number
+        imageUrl = imageUrl.slice(0,25) + imageUrlNumber + imageUrl.slice(26)
+        // set imageDiv's background image to new url string         
+        imageDiv.style.backgroundImage = imageUrl;
+    }, 3000)
+})()
+
