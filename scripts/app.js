728b3e91f42beda0aff3e175a63d5d32c38122f8
CameronHCouch <cameronhcouch@gmail.com>
2019-06-20 16:44:57 -0400

    Use regex to isolate imageUrlNumber

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 // grab carousel image from document, and locate its URL string
 let imageDiv = document.getElementById("image");
-let imageUrl = imageDiv.style.backgroundImage
+let imageUrl = imageDiv.style.backgroundImage;
 
 // determine which number image we are currently viewing
-let imageUrlNumber = parseInt(imageUrl[25]);
+let imageUrlNumber = parseInt(imageUrl.match(/\d+/)[0]);
 
 (function rotateCarousel() {
     // increment image approx every 3 seconds
@@ -15,7 +15,7 @@ let imageUrlNumber = parseInt(imageUrl[25]);
             imageUrlNumber = 0;
         }
         // replace the current imageUrlNumber with new number
-        imageUrl = imageUrl.slice(0,25) + imageUrlNumber + imageUrl.slice(26)
+        imageUrl = imageUrl.slice(0,25) + imageUrlNumber + imageUrl.slice(26);
         // set imageDiv's background image to new url string         
         imageDiv.style.backgroundImage = imageUrl;
     }, 3000)
